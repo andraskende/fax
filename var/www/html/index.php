c4aa12e92c6d8d2d124206862a9f96cebe5400df
Andras Kende <andras@gmail.com>
2021-02-12 08:10:50 -0800

    Update index.php

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -25,6 +25,10 @@ iframe {
 <br /><br />
 <iframe height="650" width="100%" src="http://192.168.1.14/fax.php"></iframe>
 <br /><br />
+<iframe height="650" width="100%" src="http://192.168.1.15/fax.php"></iframe>
+<br /><br />
+<iframe height="650" width="100%" src="http://192.168.1.16/fax.php"></iframe>
+<br /><br />
 
 </body>
 
